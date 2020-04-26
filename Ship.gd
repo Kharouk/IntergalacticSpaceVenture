83c9396ee17fc791d5ef94b77b1868af81f7aa88
Alex Kharouk <akharoukpp@gmail.com>
2020-04-26 20:18:52 +0100

    update z-index on bullets and bullet

--- Ship.gd
+++ Ship.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+const Bullet = preload("res://Bullet.tscn")
+
 export(int) var SPEED = 100
 onready var sprite := $Sprite
 
@@ -17,5 +19,16 @@ func _process(delta):
 		position.y -= SPEED * delta
 	if Input.is_action_pressed("ui_down"):
 		position.y += SPEED * delta
+	if Input.is_action_just_pressed("ui_shoot"):
+		fire_bullet()
 	
+func fire_bullet():
+	var iBullet = Bullet.instance()
 	
+	# To get the main root node (World):
+	var main = get_tree().current_scene
+	main.add_child(iBullet)
+	iBullet.z_index = 0
+	# setting the bullet's position (globally just in case it's attached to another scene) to the ship's global position 
+	var ship_position = global_position
+	iBullet.global_position = ship_position
