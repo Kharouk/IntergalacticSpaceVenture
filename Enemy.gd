1b60afbac296b4ee8d11eb2bac4cb98b396fdb04
Alex Kharouk <akharoukpp@gmail.com>
2020-04-27 20:00:32 +0100

    add guns and hit detection, enemies have life

--- Enemy.gd
+++ Enemy.gd
@@ -5,3 +5,11 @@ export(int) var HEALTH = 3
 
 func _process(delta):
 	position.x -= SPEED * delta
+
+
+func _on_Enemy_body_entered(body):
+	body.queue_free()
+	HEALTH -= 1
+	if HEALTH <= 0:
+		queue_free()
+
