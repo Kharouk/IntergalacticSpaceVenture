d6ff41e684944a287e89d34b8270d26c2ab113ed
Alex Kharouk <akharoukpp@gmail.com>
2020-04-28 17:21:03 +0100

    Set up getters and setters for score

--- World.gd
+++ World.gd
@@ -1,10 +1,15 @@
 extends Node
 class_name GameWorld
 
-var score = 0
+var score = 0 setget set_score
 var score_text = "Score = %s"
+
 onready var scoreLabel := $Score
 
-func _on_Enemy_score_up():
-	score += 10
+# no matter where we call score, it runs this function:
+func set_score(value):
+	score = value
 	scoreLabel.text = score_text % score
+
+func _on_Enemy_score_up():
+	self.score += 10
