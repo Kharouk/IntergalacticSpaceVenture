6d29dfae98d87ecfb316d9ef93afeee51cae247e
Alex Kharouk <akharoukpp@gmail.com>
2020-04-30 10:14:21 +0100

    create signal to emit on player death

--- World.gd
+++ World.gd
@@ -17,3 +17,11 @@ func set_score(value):
 func _on_Enemy_score_up():
 	# Have to add self as it might confuse itself since the setter could go into an infinite loop
 	self.score += 10
+
+
+func _on_Ship_player_death():
+	# Can quickly create a timer with the first argument
+	# Will yield until the "timeout" signal is emitted from the timer
+	yield(get_tree().create_timer(1), "timeout")
+	# we want to wait a second pretty much instead of just jumping into the game over screen, we want to see the explosions!
+	get_tree().change_scene("res://GameOverScreen.tscn")
