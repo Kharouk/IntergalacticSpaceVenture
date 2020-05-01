0fe3f570938543a4a93920ed0d840e49dad655e9
Alex Kharouk <akharoukpp@gmail.com>
2020-05-01 06:21:53 +0100

    create save data json

--- SaveAndLoad.gd
+++ SaveAndLoad.gd
@@ -1,6 +1,6 @@
 extends Node
 
-const SAVE_DATA_PATH = "res//save_data.json"
+const SAVE_DATA_PATH = "res://save_data.json"
 
 var default_save_data = {
 	highscore = 0
@@ -10,7 +10,7 @@ func save_data_to_file(save_data):
 	var json_string = to_json(save_data)
 	var save_file = File.new()
 	save_file.open(SAVE_DATA_PATH, File.WRITE)
-	save_file.store_line(json_string)
+	save_file.store_string(json_string)
 	save_file.close()
 
 func load_data_from_file():
